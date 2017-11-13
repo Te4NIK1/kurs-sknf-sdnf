d3c0da2994cb45bbbfa40abf679b789be97ea373
Sergej <Te4NIK1@gmail.com>
2017-11-13 07:59:43 +0300

    v 2.0
    my funcs replace with c-default

--- main.cpp
+++ main.cpp
@@ -1,200 +1,290 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-string s; //input expression
-bool correct_input = 1; // used for check input expression. If  == 0 - do notnig
-//result functions
-string sknf = "";
-string sdnf = "";
+string s; //
+bool correct_input; // used for check input expression. If  == 0 - do notnig
+string sknf, sdnf; //result functions
+int kolX; // number of variables (need for make true-table)
+string X[11];  //values of variables
 
-int kolX = 0; // number of variables (need for make true-table)
-bool X[10];  //values of variables
+string indstring(int ind)
+{
+	switch (ind) {
+		case 1 : return "X1";
+		case 2 : return "X2";
+		case 3 : return "X3";
+		case 4 : return "X4";
+		case 5 : return "X5";
+		case 6 : return "X6";
+		case 7 : return "X7";
+		case 8 : return "X8";
+		case 9 : return "X9";
+		case 10 : return "X10";
+	}
+	return "Error";
+}
+
+bool check(string input)
+{
+	for(int i = 0; i < input.size(); i++) {
+		if (
+		    input[i] != 'X' &&
+		    input[i] != '-' && 
+			input[i] != '>' && 
+			input[i] != '<' && 
+			input[i] != '^' && 
+			input[i] != 'v' && 
+			input[i] != ')' &&
+			input[i] != '(' &&
+			!isdigit(input[i])) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int count(string input)
+{
+	size_t found;
+	
+	found = input.find("X10");
+  	if (found != string::npos)
+		return 10;	
+		
+	found = input.find("X9");
+  	if (found != string::npos)
+		return 9;	
+		
+	found = input.find("X8");
+  	if (found != string::npos)
+		return 8;	
+		
+	found = input.find("X7");
+  	if (found != string::npos)
+		return 7;	
+		
+	found = input.find("X6");
+  	if (found != string::npos)
+		return 6;	
+		
+	found = input.find("X5");
+  	if (found != string::npos)
+		return 5;	
+		
+	found = input.find("X4");
+  	if (found != string::npos)
+		return 4;	
+		
+	found = input.find("X3");
+  	if (found != string::npos)
+		return 3;	
+		
+	found = input.find("X2");
+  	if (found != string::npos)
+		return 2;	
+		
+	found = input.find("X1");
+  	if (found != string::npos)
+		return 1;
+		
+	return 0;		
+}
 
 string change(string input) // function for change variables to their values (smpl: x1 v x2 goes to 0v1)
 {
-	string result = "";
-	for(int i=0; i<input.size();)
-	{
-		if(input[i]==' ') //drop all spaces (we dont need them)
-		{
-			i++;
+	size_t found;
+	for(;;) {
+		found = input.find("X1");
+		if (found != string::npos) {
+			input.replace(found, 2, X[1]);	
 			continue;
-		}
-		if(input[i]=='X') //change variable to current values
-		{
-			bool value = X[int(input[i+1])-'1'];
-			result+=char( value+'0' );
-			i+=2;
+	    }
+		found = input.find("X2");
+		if (found != string::npos) {
+			input.replace(found, 2, X[2]);	
 			continue;
-		}	
-		else
-		{
-			result+=input[i]; //if no variable - just add char to result
-			i++;
-		}
+	    }
+		found = input.find("X3");
+		if (found != string::npos) {
+			input.replace(found, 2, X[3]);	
+			continue;
+	    }
+		found = input.find("X4");
+		if (found != string::npos) {
+			input.replace(found, 2, X[4]);	
+			continue;
+	    }
+		found = input.find("X5");
+		if (found != string::npos) {
+			input.replace(found, 2, X[5]);	
+			continue;
+	    }
+		found = input.find("X6");
+		if (found != string::npos) {
+			input.replace(found, 2, X[6]);	
+			continue;
+	    }
+		found = input.find("X7");
+		if (found != string::npos) {
+			input.replace(found, 2, X[7]);	
+			continue;
+	    }
+		found = input.find("X8");
+		if (found != string::npos) {
+			input.replace(found, 2, X[8]);	
+			continue;
+	    }
+		found = input.find("X9");
+		if (found != string::npos) {
+			input.replace(found, 2, X[9]);	
+			continue;
+	    }
+		found = input.find("X10");
+		if (found != string::npos) {
+			input.replace(found, 3, X[10]);	
+			continue;
+	    }
+		break;
 	}
-	return result;
+	return input;
 }
 
-bool parse(string input)
+string parse(string input)
 {
-	string result = "";
-	for(bool do_something=1; do_something!=0;)
-	{
-		//cout <<"current: "<<input <<endl;
-		
-		do_something = 0;
-
+	size_t found;
+	for(;;) {
+		//cout <<input<<endl;
 		//drop -
-		result = "";
-		for(int i=0; i<input.size();) {
-			if ( (i<input.size()-1) &&
-			     (input[i]=='-')  &&
-				 (input[i+1]=='0' || input[i+1]=='1')) // -X
-			{
-				do_something = 1;
-				if(input[i+1]=='0')
-					result+='1';
-				else
-					result+='0';	
-				i+=2;
-			}
-			else {
-				result += input[i];
-				i++;
-			}
+		found = input.find("-0");
+  		if (found != string::npos) {
+			input.replace(found, 2, "1");	
+			continue;
 		}
-		if(do_something) {
-			input = result;
-			result = "";
+		
+		found = input.find("-1");
+  		if (found != string::npos) {
+			input.replace(found, 2, "0");	
 			continue;
 		}
 		
 		//drop brackets
-		result = "";
-		for(int i=0; i<input.size();) {
-			if ( (i<input.size()-2) &&
-			     (input[i]=='(') &&
-			    ((input[i+1]=='0') || (input[i+1]=='1')) && 
-				 (input[i+2]==')')) // (1) or (0)
-			{
-				do_something = 1;
-				result += input[i+1];
-				i+=3;
-			}
-			else
-			{
-				result += input[i];
-				i++;
-			}
+		found = input.find("(0)");
+  		if (found != string::npos) {
+			input.replace(found, 3, "0");	
+			continue;
 		}
-		if(do_something) goto exit;
+		
+		found = input.find("(1)");
+  		if (found != string::npos) {
+			input.replace(found, 3, "1");	
+			continue;
+		}
 		
 		//drop ->
-		result = "";
-		for(int i=0; i<input.size();) {
-			if ( (i<input.size()-3) &&
-			     (input[i]=='0' || input[i]=='1')  &&
-			     (input[i+1]=='-') && 
-			     (input[i+2]=='>') && 
-				 (input[i+3]=='0' || input[i+3]=='1')) // X->X
-			{
-				do_something = 1;
-				if(input[i]=='1' && input[i+3]=='0')
-					result+='0';
-				else
-					result+='1';	
-				i+=4;
-			}
-			else {
-				result += input[i];
-				i++;
-			}
+		found = input.find("0->0");
+  		if (found != string::npos) {
+			input.replace(found, 4, "1");	
+			continue;
 		}
-		if(do_something) goto exit;
+		
+		found = input.find("0->1");
+  		if (found != string::npos) {
+			input.replace(found, 4, "1");	
+			continue;
+		}
+		
+		found = input.find("1->0");
+  		if (found != string::npos) {
+			input.replace(found, 4, "0");	
+			continue;
+		}
+		
+		found = input.find("1->1");
+  		if (found != string::npos) {
+			input.replace(found, 4, "1");	
+			continue;
+		}
 		
 		//drop <>
-		result = "";
-		for(int i=0; i<input.size();) {
-			if ( (i<input.size()-3) &&
-			     (input[i]=='0' || input[i]=='1')  &&
-			     (input[i+1]=='<') && 
-			     (input[i+2]=='>') && 
-				 (input[i+3]=='0' || input[i+3]=='1')) // X<>X
-			{
-				do_something = 1;
-				if(input[i]==input[i+3])
-					result+='1';
-				else
-					result+='0';	
-				i+=4;
-			}
-			else {
-				result += input[i];
-				i++;
-			}
-		}	
-		if(do_something) goto exit;
+		found = input.find("0<>0");
+  		if (found != string::npos) {
+			input.replace(found, 4, "1");	
+			continue;
+		}
 		
+		found = input.find("0<>1");
+  		if (found != string::npos) {
+			input.replace(found, 4, "0");	
+			continue;
+		}
+		
+		found = input.find("1<>0");
+  		if (found != string::npos) {
+			input.replace(found, 4, "0");	
+			continue;
+		}
+		
+		found = input.find("1<>1");
+  		if (found != string::npos) {
+			input.replace(found, 4, "1");	
+			continue;
+		}
+
 		//drop ^
-		result = "";
-		for(int i=0; i<input.size();) {
-			if ( (i<input.size()-2) &&
-			     (input[i]=='0' || input[i]=='1')  &&
-			     (input[i+1]=='^') && 
-				 (input[i+2]=='0' || input[i+2]=='1')) // XvX
-			{
-				do_something = 1;
-				if(input[i]=='1' && input[i+2]=='1')
-					result+='1';
-				else
-					result+='0';	
-				i+=3;
-			}
-			else {
-				result += input[i];
-				i++;
-			}
-		}	
-		if(do_something) goto exit;
+		found = input.find("0^0");
+  		if (found != string::npos) {
+			input.replace(found, 3, "0");	
+			continue;
+		}
+		
+		found = input.find("0^1");
+  		if (found != string::npos) {
+			input.replace(found, 3, "0");	
+			continue;
+		}
+		
+		found = input.find("1^0");
+  		if (found != string::npos) {
+			input.replace(found, 3, "0");	
+			continue;
+		}
+		
+		found = input.find("1^1");
+  		if (found != string::npos) {
+			input.replace(found, 3, "1");	
+			continue;
+		}
 		
 		//drop v
-		result = "";
-		for(int i=0; i<input.size();) {
-			if ( (i<input.size()-2) &&
-			     (input[i]=='0' || input[i]=='1')  &&
-			     (input[i+1]=='v') && 
-				 (input[i+2]=='0' || input[i+2]=='1')) // XvX
-			{
-				do_something = 1;
-				if(input[i]=='0' && input[i+2]=='0')
-					result+='0';
-				else
-					result+='1';	
-				i+=3;
-			}
-			else {
-				result += input[i];
-				i++;
-			}
+		found = input.find("0v0");
+  		if (found != string::npos) {
+			input.replace(found, 3, "0");	
+			continue;
 		}
-		if(do_something) goto exit;				
-exit:
-		input = result;
-		result = "";
-		if (!do_something)
-			break;		
+		
+		found = input.find("0v1");
+  		if (found != string::npos) {
+			input.replace(found, 3, "1");	
+			continue;
+		}
+		
+		found = input.find("1v0");
+  		if (found != string::npos) {
+			input.replace(found, 3, "1");	
+			continue;
+		}
+		
+		found = input.find("1v1");
+  		if (found != string::npos) {
+			input.replace(found, 3, "1");	
+			continue;
+		}
+		break;		
 	}
-	
-	if(input.size()!=1) // result not 1 or 0
-	{
-		correct_input = 0;
-		return 0;
-	}	
-	return (bool)(input[0]-'0');	
+	return input;	
 }
 
 void dfs(int ind)
@@ -202,88 +292,93 @@ void dfs(int ind)
 	if(!correct_input)
 		return;
 		
-	if(ind==kolX)
-	{
+	if(ind == kolX + 1) {
 		//we got all x[i] values
 		//cout <<"end dfs "<<change(s)<<endl;
-		if (parse(change(s)) == 0) {
-			if(sknf.size()>0)
-				sknf+=" ^ ";
-			sknf+="(";	
-			for (int i=0; i<kolX; i++) //(x1 v -x2 v x3)
-			{
-				if(i>0)
-					sknf+=" v ";
-				if(X[i]==1)
-					sknf+="-";
-				sknf+="X";
-				sknf+=(char)(i+'1');
-			}
-			sknf+=")";	
+		string result = parse(change(s));
+
+		if (result != "0" && result != "1") {
+			correct_input = 0;
+			return;
 		}
-		else {
-			if(sdnf.size()>0)
-				sdnf+=" v ";
-			sdnf+="(";	
-			for (int i=0; i<kolX; i++) //(-x1 ^ x2 v -x3)
-			{
-				if(i>0)
-					sdnf+=" ^ ";
-				if(X[i]==0)
-					sdnf+="-";
-				sdnf+="X";
-				sdnf+=(char)(i+'1');
+		
+		if(result == "0") {
+			if(sknf.size() > 0)
+				sknf += " ^ ";
+				
+			sknf += "(";	
+			for(int i = 1; i <= kolX; i++) { //(x1 v -x2 v x3)
+				if(X[i] == "1")
+					sknf += "-";
+				sknf += indstring(i);
+				if(i < kolX)
+					sknf += " v ";
 			}
-			sdnf+=")";	
+			sknf += ")";	
+		}
+		
+		if(result == "1") {
+			if(sdnf.size() > 0)
+				sdnf += " v ";
+				
+			sdnf += "(";	
+			for (int i = 1; i <= kolX; i++) { //(-x1 ^ x2 v -x3)
+				if(X[i] == "0")
+					sdnf += "-";
+				sdnf += indstring(i);
+				if(i < kolX)
+					sdnf += " ^ ";
+			}
+			sdnf += ")";	
 		}
 		return;
 	}
-	X[ind]=0;
-	dfs(ind+1);
-	X[ind]=1;
-	dfs(ind+1);
+	
+	X[ind] = '0';
+	dfs(ind + 1);
+	
+	X[ind] = '1';
+	dfs(ind + 1);
 }
 
 int main(int argc, char** argv) {
 	
-	cout <<"F=";
-	getline(cin, s);
-	//s = "(X1 -> X2) ^ ((-X2 -> X3) -> -X1)";
+	for(;;) {
+		cout <<"F=";
+		getline(cin, s);
+		//s = "(X1 -> X2) ^ ((-X2 -> X3) -> -X1)";
 	
-	//count variables
-	for(int i=0; i<s.size(); i++)
-	{
-		if (!(
-		    s[i]=='X' ||
-		    s[i]=='-' || 
-			s[i]=='>' || 
-			s[i]=='<' || 
-			s[i]!=' ' || 
-			s[i]!=')' ||
-			s[i]!='(' ||
-			(s[i]>='1' && s[i]>='9')))
-		{
-			correct_input = 0;
-			break;	
+		for(;;) { //drop all spaces
+			size_t found = s.find(" ");
+  			if (found != string::npos) {
+				s.erase(found, 1);	
+				continue;
+			}
+			break;
 		}
-		if(i<s.size()-1 && s[i]=='X')
-		{
-			if(s[i+1]<'1' || s[i+1]>'9')
-				correct_input = 0;
-			else	
-				kolX = max(kolX, int(s[i+1]-'0'));
+	
+		correct_input = check(s);
+		kolX = count(s);
+	
+		sknf = "";
+		sdnf = "";
+		dfs(1);
+	
+		if(!correct_input)
+			cout <<"Incorrect expression"<<endl;
+		else {
+			cout <<"SKNF: "<<sknf<<endl<<endl;
+			cout <<"SDNF: "<<sdnf<<endl<<endl;
 		}
-	}
-	dfs(0);
-	if(!correct_input)
-		cout <<"Incorrect expression"<<endl;
-	else
-	{
-		cout <<"SKNF: "<<sknf<<endl<<endl;
-		cout <<"SDNF: "<<sdnf<<endl<<endl;
-	}
-	cout <<"Press Enter to quit\n";
-	getline(cin,s);
-		
+	
+		for(;;) {
+			cout <<"Continue Y/N?"<<endl;
+			getline(cin, s);
+			if(s == "Y")
+				break;
+			if(s == "N")
+				return 0;
+		}
+	}	
 	return 0;
 }
